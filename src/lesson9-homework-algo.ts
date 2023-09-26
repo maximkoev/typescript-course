0de1d4c6b6333f01a38d0a46ef3cb41f7de2a832
maksym koiev <icmaksym.koiev@americanwell.com>
2023-09-26 23:22:27 +0300

    hw9

---
+++ src/lesson9-homework-algo.ts
@@ -0,0 +1,58 @@
+function exerciseExtra2() {
+    /**
+     * Write a program that prints the integers from 1 to 100 (inclusive).
+     * But:
+     *  - for multiples of three, print Fizz (instead of the number)
+     *  - for multiples of five, print Buzz (instead of the number)
+     *  - for multiples of both three and five, print FizzBuzz (instead of the number)
+     */
+
+    function verifyFizzOrBuzz(i: number) {
+        if (i === 0) return i
+        if (i % 3 == 0) return 'Fizz'
+        if (i % 5 == 0) return 'Buzz'
+        else return i
+    }
+
+    function fizzBuzz() {
+        // TODO: add your code here
+        for (let i = 0; i < 101; i++) {
+            console.log(verifyFizzOrBuzz(i))
+        }
+    }
+
+    // fizzBuzz();
+
+    /**
+     * 1
+     * 2
+     * Fizz
+     * 4
+     * Buzz
+     * ...
+     */
+
+    // TODO: convert fizzBuzz to generate a string instead of printing to console
+    function fizzBuzzToString(): string {
+        let res: string = ''
+        for (let i = 0; i < 101; i++) {
+            res = res.concat(verifyFizzOrBuzz(i).toString())
+        }
+        return res
+    }
+
+    console.log(fizzBuzzToString())
+
+    fizzBuzzToString();
+    // TODO: write a test to validate fizzBuzz output using console.assert
+    console.assert(typeof fizzBuzzToString() === 'string');
+    console.assert(verifyFizzOrBuzz(3) === 'Fizz', 'should return Fizz')
+    console.assert(verifyFizzOrBuzz(5) === 'Buzz', 'should return buzz')
+    console.assert(verifyFizzOrBuzz(4) === 4, 'should return 4')
+    console.assert(verifyFizzOrBuzz(1) === 1, 'should return 1')
+    console.assert(verifyFizzOrBuzz(2) === 2, 'should return 2')
+    console.assert(verifyFizzOrBuzz(6) === 6, 'should return Fizz for 6')
+    console.assert(fizzBuzzToString().split('')[0] === '0', 'first element should be 0')
+}
+
+exerciseExtra2();
