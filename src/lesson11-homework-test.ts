753eb71572cffd601d31e2374aa1f2f56e6ed59e
maksym koiev <icmaksym.koiev@americanwell.com>
2023-10-05 01:26:54 +0300

    hw11

--- src/lesson11-homework-test.ts
+++ src/lesson11-homework-test.ts
@@ -6,249 +6,294 @@ declare const it: any;
 declare const xit: any;
 
 describe("Users class", () => {
-  const { expect } = chai;
-
-  it("should be defined", () => {
-    expect(Users).to.not.be.undefined;
-  });
-
-  it("should define constructor that accepts users list", () => {
-    const users = new Users(data.users);
-
-    expect(users.list).to.equal(data.users);
-  });
-
-  describe("getUsersNames method", () => {
-    // - Написати метод що повератє масив імен усіх користувачів у форматі "John, Smith" ("firstName, lastName")
+    const {expect} = chai;
 
     it("should be defined", () => {
-      const users = new Users(data.users);
-
-      expect(users.getUsersNames).to.not.be.undefined;
+        expect(Users).to.not.be.undefined;
     });
 
-    it("should return list of user names", () => {
-      const mockData = [
-        { firstName: "Bob", lastName: "Smith" },
-        { firstName: "Jack", lastName: "Chan" },
-      ];
+    it("should define constructor that accepts users list", () => {
+        const users = new Users(data.users);
 
-      const users = new Users(mockData);
-
-      expect(users.getUsersNames()).to.deep.equal(["Bob, Smith", "Jack, Chan"]);
+        expect(users.list).to.equal(data.users);
     });
 
-    xit("should work for blank data", () => {
-      const users = new Users([]);
+    describe("getUsersNames method", () => {
+        // - Написати метод що повератє масив імен усіх користувачів у форматі "John, Smith" ("firstName, lastName")
 
-      // - Виправити getUsersNames так, що цей юніт-тест проходив
-      expect(users.getUsersNames()).to.deep.equal([]);
+        it("should be defined", () => {
+            const users = new Users(data.users);
+
+            expect(users.getUsersNames).to.not.be.undefined;
+        });
+
+        it("should return list of user names", () => {
+            const mockData = [
+                {firstName: "Bob", lastName: "Smith"},
+                {firstName: "Jack", lastName: "Chan"},
+            ] as any as TUsers;
+
+            const users = new Users(mockData);
+
+            expect(users.getUsersNames()).to.deep.equal(["Bob, Smith", "Jack, Chan"]);
+        });
+
+        it("should work for blank data", () => {
+            const users = new Users([]);
+
+            // - Виправити getUsersNames так, що цей юніт-тест проходив
+            expect(users.getUsersNames()).to.deep.equal([]);
+        });
+
+        it("should work for real data", () => {
+            const expected = ['Terry, Medhurst', 'Sheldon, Quigley', 'Terrill, Hills', 'Miles, Cummerata', 'Mavis, Schultz', 'Alison, Reichert', 'Oleta, Abbott', 'Ewell, Mueller', 'Demetrius, Corkery', 'Eleanora, Price', 'Marcel, Jones', 'Assunta, Rath', 'Trace, Douglas', 'Enoch, Lynch', 'Jeanne, Halvorson', 'Trycia, Fadel', 'Bradford, Prohaska', 'Arely, Skiles', 'Gust, Purdy', 'Lenna, Renner', 'Doyle, Ernser', 'Tressa, Weber', "Felicity, O'Reilly", 'Jocelyn, Schuster', 'Edwina, Ernser', 'Griffin, Braun', 'Piper, Schowalter', 'Kody, Terry', 'Macy, Greenfelder', 'Maurine, Stracke'];
+            const users = new Users(data.users);
+            // - Дописати цей юніт-тест, так щоб використовувалися data.users
+            expect(users.getUsersNames()).to.deep.equal(expected);
+        });
     });
 
-    xit("should work for real data", () => {
-      const users = new Users(data.users);
+    describe("updateUsersAge method", () => {
+        // - Написати метод що виправляє вік користувачів, апдейтить дані користувачів правильним віком (age) на поточну дату, 2023 рік (використати день народження birthDate для обчислення віку)
 
-      // - Дописати цей юніт-тест, так щоб використовувалися data.users
-      expect(users.getUsersNames()).to.deep.equal(["Terry, Medhurst", "..."]);
-    });
-  });
+        it("should be defined", () => {
+            const users = new Users(data.users);
 
-  describe("updateUsersAge method", () => {
-    // - Написати метод що виправляє вік користувачів, апдейтить дані користувачів правильним віком (age) на поточну дату, 2023 рік (використати день народження birthDate для обчислення віку)
+            expect(users.updateUsersAge).to.not.be.undefined;
+        });
 
-    it("should be defined", () => {
-      const users = new Users(data.users);
+        it("should update users age", () => {
+            const mockData = [
+                {
+                    firstName: "Bob",
+                    lastName: "Smith",
+                    age: 18,
+                    birthDate: "2003-02-13",
+                },
+                {
+                    firstName: "Jack",
+                    lastName: "Chan",
+                    age: 28,
+                    birthDate: "1993-02-13",
+                },
+            ] as TUsers;
 
-      expect(users.updateUsersAge).to.not.be.undefined;
-    });
+            const users = new Users(mockData);
 
-    it("should update users age", () => {
-      const mockData = [
-        {
-          firstName: "Bob",
-          lastName: "Smith",
-          age: 18,
-          birthDate: "2003-02-13",
-        },
-        {
-          firstName: "Jack",
-          lastName: "Chan",
-          age: 28,
-          birthDate: "1993-02-13",
-        },
-      ];
+            const usersAges = users.updateUsersAge().map((user) => user.age);
 
-      const users = new Users(mockData);
+            expect(usersAges).to.deep.equal([20, 30]);
+        });
 
-      users.updateUsersAge();
+        it("should work for blank data", () => {
+            const users = new Users([]);
 
-      const usersAges = users.list.map((user) => user.age);
+            users.updateUsersAge();
 
-      expect(usersAges).to.deep.equal([20, 30]);
-    });
+            const usersAges = users.list.map((user) => user.age);
 
-    xit("should work for blank data", () => {
-      const users = new Users([]);
+            // - Виправити updateUsersAge так, що цей юніт-тест проходив
+            expect(usersAges).to.deep.equal([]);
+        });
 
-      users.updateUsersAge();
+        it("should work for real data", () => {
+            const expectedData = [23, 20, 31, 54, 55, 54, 41, 59, 52, 65, 62, 33, 56, 44, 27, 60, 48, 65, 34, 43, 40, 36, 56, 57, 23, 58, 40, 44, 47, 59];
+            const testData: TUsers = data.users;
+            const users = new Users(testData);
+            const usersAges = users.updateUsersAge().map((user) => user.age);
 
-      const usersAges = users.list.map((user) => user.age);
-
-      // - Виправити updateUsersAge так, що цей юніт-тест проходив
-      expect(usersAges).to.deep.equal([]);
-    });
-
-    xit("should work for real data", () => {
-      const users = new Users(data.users);
-
-      users.updateUsersAge();
-
-      const usersAges = users.list.map((user) => user.age);
-
-      // - Виправити updateUsersAge так, що цей юніт-тест проходив
-      // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити 1, 2, 3 на правильні дані
-      expect(usersAges).to.deep.equal([1, 2, 3]);
-    });
-  });
-
-  describe("getUsersFromUkraine method", () => {
-    // - Написати функцію що повератє список користувачів з України (номер телефону +380).
-
-    it("should be defined", () => {
-      const users = new Users(data.users);
-
-      expect(users.getUsersFromUkraine).to.not.be.undefined;
+            // - Виправити updateUsersAge так, що цей юніт-тест проходив
+            // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити 1, 2, 3 на правильні дані
+            expect(usersAges).to.deep.equal(expectedData);
+        });
     });
 
-    it("should return list of users from Ukraine", () => {
-      const mockData = [
-        { firstName: "Bob", lastName: "Smith", phone: "+7 813 117 7139" },
-        { firstName: "Jack", lastName: "Chan", phone: "+7 813 117 7139" },
-        { firstName: "Ove", lastName: "Malyk", phone: "+3809525426549" },
-        { firstName: "Myk", lastName: "Franko", phone: "+3809625426549" },
-      ];
+    describe("getUsersFromUkraine method", () => {
+        // - Написати функцію що повератє список користувачів з України (номер телефону +380).
 
-      const users = new Users(mockData);
+        it("should be defined", () => {
+            const users = new Users(data.users);
 
-      const usersNamesAndPhones = users
-        .getUsersFromUkraine()
-        .map(({ firstName, lastName, phone }) => ({
-          firstName,
-          lastName,
-          phone,
-        }));
+            expect(users.getUsersFromUkraine).to.not.be.undefined;
+        });
 
-      expect(usersNamesAndPhones.length).to.equal(2);
+        it("should return list of users from Ukraine", () => {
+            const mockData = [
+                {firstName: "Bob", lastName: "Smith", phone: "+7 813 117 7139"},
+                {firstName: "Jack", lastName: "Chan", phone: "+7 813 117 7139"},
+                {firstName: "Ove", lastName: "Malyk", phone: "+3809525426549"},
+                {firstName: "Myk", lastName: "Franko", phone: "+3809625426549"},
+            ] as TUsers;
 
-      expect(usersNamesAndPhones).to.deep.include({
-        firstName: "Myk",
-        lastName: "Franko",
-        phone: "+3809625426549",
-      });
+            const users = new Users(mockData);
 
-      expect(usersNamesAndPhones).to.deep.include({
-        firstName: "Ove",
-        lastName: "Malyk",
-        phone: "+3809525426549",
-      });
+            const usersNamesAndPhones = users
+                .getUsersFromUkraine()
+                .map(({firstName, lastName, phone}) => ({
+                    firstName,
+                    lastName,
+                    phone,
+                }));
+
+            expect(usersNamesAndPhones.length).to.equal(2);
+
+            expect(usersNamesAndPhones).to.deep.include({
+                firstName: "Myk",
+                lastName: "Franko",
+                phone: "+3809625426549",
+            });
+
+            expect(usersNamesAndPhones).to.deep.include({
+                firstName: "Ove",
+                lastName: "Malyk",
+                phone: "+3809525426549",
+            });
+        });
+
+        it("should work for real data", () => {
+            const users = new Users(data.users);
+
+            const usersNamesAndPhones = users
+                .getUsersFromUkraine()
+                .map(({firstName, lastName, phone}) => ({
+                    firstName,
+                    lastName,
+                    phone,
+                }));
+
+            expect(usersNamesAndPhones.length).to.equal(1);
+
+            // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
+            expect(usersNamesAndPhones).to.deep.include({
+                firstName: "Assunta",
+                lastName: "Rath",
+                phone: "+380 962 542 6549",
+            });
+        });
     });
 
-    xit("should work for real data", () => {
-      const users = new Users(data.users);
+    describe("getStatePostalCodes method", () => {
+        // - Написати функцію що на основі даних усіх користувачів повератє масив штатів і їх поштових індексів у вигляді масиву обєктів [{"name": "AK", "postalCodes": ["99503", "..."]}]. Штати не повинні повторюватися.
 
-      const usersNamesAndPhones = users
-        .getUsersFromUkraine()
-        .map(({ firstName, lastName, phone }) => ({
-          firstName,
-          lastName,
-          phone,
-        }));
+        it("should be defined", () => {
+            const users = new Users(data.users);
 
-      expect(usersNamesAndPhones.length).to.equal(1);
+            expect(users.getStatePostalCodes).to.not.be.undefined;
+        });
 
-      // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
-      expect(usersNamesAndPhones).to.deep.include({
-        firstName: "Assunta",
-        lastName: "Rath",
-        phone: "+380 962 542 6549",
-      });
-    });
-  });
+        it("should return list of postal codes for all states", () => {
+            const mockData = [
+                {
+                    firstName: "Joe",
+                    address: {
+                        postalCode: "80003",
+                        state: "CO",
+                    },
+                },
+                {
+                    firstName: "Bob",
+                    address: {
+                        postalCode: "80004",
+                        state: "CO",
+                    },
+                },
+                {
+                    firstName: "Mike",
+                    address: {
+                        postalCode: "10001",
+                        state: "AK",
+                    },
+                },
+            ] as TUsers;
 
-  describe("getStatePostalCodes method", () => {
-    // - Написати функцію що на основі даних усіх користувачів повератє масив штатів і їх поштових індексів у вигляді масиву обєктів [{"name": "AK", "postalCodes": ["99503", "..."]}]. Штати не повинні повторюватися.
+            const users = new Users(mockData);
 
-    it("should be defined", () => {
-      const users = new Users(data.users);
+            expect(users.getStatePostalCodes()).to.deep.equal([
+                {name: "CO", postalCodes: ["80003", "80004"]},
+                {name: "AK", postalCodes: ["10001"]},
+            ]);
+        });
 
-      expect(users.getStatePostalCodes).to.not.be.undefined;
-    });
-
-    it("should return list of postal codes for all states", () => {
-      const mockData = [
-        {
-          firstName: "Joe",
-          address: {
-            postalCode: "80003",
-            state: "CO",
-          },
-        },
-        {
-          firstName: "Bob",
-          address: {
-            postalCode: "80004",
-            state: "CO",
-          },
-        },
-        {
-          firstName: "Mike",
-          address: {
-            postalCode: "10001",
-            state: "AK",
-          },
-        },
-      ];
-
-      const users = new Users(mockData);
-
-      expect(users.getStatePostalCodes()).to.deep.equal([
-        { name: "CO", postalCodes: ["80003", "80004"] },
-        { name: "AK", postalCodes: ["10001"] },
-      ]);
-    });
-
-    xit("should work for real data", () => {
-      const users = new Users(data.users);
-
-      // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
-      // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити XXX на правильні дані
-      expect(users.getStatePostalCodes()).to.deep.equal([
-        { name: "XXX", postalCodes: ["XXX", "XXX"] },
-        { name: "XXX", postalCodes: ["XXX", "XXX"] },
-        // ... Це місце де має бути ваш код
-      ]);
-    });
-  });
-
-  describe("getAverageWomenAge method", () => {
-    // - Написати функцію що повератє середній вік всіх жінок ("gender": "female")
-
-    xit("should be defined", () => {
-      const users = new Users(data.users);
-
-      // expect(users.getMediumWomenAge).to.not.be.undefined;
+        it("should work for real data", () => {
+            const users = new Users(data.users);
+            users.getStatePostalCodes().forEach(e => console.log(`{name: "${e.name}", postalCodes: [${e.postalCodes} ]}`));
+            // @ts-ignore
+            const exp = [
+                {name: "DC", postalCodes: ['20020', '20001']},
+                {name: "KY", postalCodes: ['40219', '40206', '40208']},
+                {name: "CA", postalCodes: ['95945', '94591', '93908', '93645']},
+                // @ts-ignore
+                {name: "CT", postalCodes: ['06040', '06042']},
+                // @ts-ignore
+                {name: "VT", postalCodes: ['05452', '05037']},
+                {name: "AZ", postalCodes: ['85306', '85305']},
+                {name: "TN", postalCodes: ['37209', '37206', '37076', '37211', '37013']},
+                {name: "CO", postalCodes: ['80003']},
+                {name: "MA", postalCodes: ['02664']},
+                {name: "AR", postalCodes: ['72704', '72703']},
+                {name: "AL", postalCodes: ['36108', '36111']},
+                {name: "GA", postalCodes: ['31415']},
+                {name: "AK", postalCodes: ['99503']}
+            ];
+            // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
+            // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити XXX на правильні дані
+            expect(users.getStatePostalCodes()).to.deep.equal(exp);
+        });
     });
 
-    xit("should work for real data", () => {
-      const users = new Users(data.users);
+    describe("getAverageWomenAge method", () => {
+        // - Написати функцію що повератє середній вік всіх жінок ("gender": "female")
 
-      // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
-      // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити 12345 на правильні дані
-      // expect(users.getMediumWomenAge()).to.deep.equal(12345);
+        it("should be defined", () => {
+            const users = new Users(data.users);
+
+            expect(users.getMediumWomenAge).to.not.be.undefined;
+        });
+
+        it("should work for real data", () => {
+            const users = new Users(data.users);
+
+            // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
+            // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити 12345 на правильні дані
+            expect(users.getMediumWomenAge()).to.deep.equal(36.15);
+        });
     });
-  });
 
-  // Написати юніт-тести для наступних методів
-  // getMostCommonWoomanHairColor - метод що повератє найбільш поширений колір волося серед жінок
-  // getMostCommonManBlodType - метод що повератє найбільш поширениу групу крові серед чоловіків
+    // Написати юніт-тести для наступних методів
+    // getMostCommonWoomanHairColor - метод що повератє найбільш поширений колір волося серед жінок
+    describe("getMostCommonWoomanHairColor method", () => {
+        // - Написати функцію що повератє середній вік всіх жінок ("gender": "female")
+
+        it("should be defined", () => {
+            const users = new Users(data.users);
+
+            expect(users.getMostCommonWoomanHairColor).to.not.be.undefined;
+        });
+
+        it("should work for real data", () => {
+            const users = new Users(data.users);
+
+            // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
+            // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити 12345 на правильні дані
+            expect(users.getMostCommonWoomanHairColor()).to.deep.equal('Black');
+        });
+    });
+    // getMostCommonManBlodType - метод що повератє найбільш поширениу групу крові серед чоловіків
+    describe("getMostCommonManBlodType method", () => {
+        // - Написати функцію що повератє середній вік всіх жінок ("gender": "female")
+
+        it("should be defined", () => {
+            const users = new Users(data.users);
+
+            expect(users.getMostCommonManBlodType).to.not.be.undefined;
+        });
+
+        it("should work for real data", () => {
+            const users = new Users(data.users);
+
+            // - Виправити метод getUsersFromUkraine так, щоб цей юніт-тест проходив
+            // - Дописати цей юніт-тест, так щоб використовувалися data.users - замінити 12345 на правильні дані
+            expect(users.getMostCommonManBlodType()).to.equal('A−');
+        });
+    });
 });
--- src/lesson8-homework-algo.ts
+++ src/lesson8-homework-algo.ts
@@ -1,39 +1,39 @@
 // This is an algorithmic problem - use your algorithmic skills and typescript knowledge to solve it
-function exerciseExtra1() {
-    // TODO: create a function to determine if two strings are an anagram
-    // HINT: A word is an anagram of another if you can rearrange its characters to produce the second word.
-    function areAnagrams(s1: string, s2: string): boolean {
-        const s1Arr = s1.split('');
-        const s2Arr = s2.split('');
-        let isAnagram: boolean = true;
-        if (s1Arr.length != s2Arr.length) {
-            isAnagram = false
-            return isAnagram;
-        }
-        for (let i = 0; i < s1Arr.length; i++) {
-            const currentChar = s1Arr[i];
-            const charIndex = s2Arr.indexOf(currentChar)
-            if (charIndex < 0) {
-                isAnagram = false
-                break;
-            }
-            s2Arr.splice(charIndex, 1)
-        }
-
-        return isAnagram;
-    }
-
-    console.assert(!areAnagrams("abc", "abcd"), "Length check is failed");
-    console.assert(areAnagrams("listen", "silent"), "listen&silent are not anagram");
-    console.assert(areAnagrams("abc", "cba"), "abc&cba are not anagram");
-    console.assert(!areAnagrams("abc", "cbd"), "abc&cbd are anagram");
-    console.assert(areAnagrams("", ""), "empty strings");
-    console.assert(!areAnagrams("abcсс", "abcaa"), "abcc and abcaa are not anagrams")
-    console.assert(!areAnagrams("abca", "abcc"), "abcc and abcaa are not anagrams")
-    console.assert(!areAnagrams("abcaa", "abccc"), "abcc and abcaa are not anagrams")
-
-
-    // HINT: consider exercise33 for code reuse ideas
-}
-
-exerciseExtra1();
+// function exerciseExtra1() {
+//     // TODO: create a function to determine if two strings are an anagram
+//     // HINT: A word is an anagram of another if you can rearrange its characters to produce the second word.
+//     function areAnagrams(s1: string, s2: string): boolean {
+//         const s1Arr = s1.split('');
+//         const s2Arr = s2.split('');
+//         let isAnagram: boolean = true;
+//         if (s1Arr.length != s2Arr.length) {
+//             isAnagram = false
+//             return isAnagram;
+//         }
+//         for (let i = 0; i < s1Arr.length; i++) {
+//             const currentChar = s1Arr[i];
+//             const charIndex = s2Arr.indexOf(currentChar)
+//             if (charIndex < 0) {
+//                 isAnagram = false
+//                 break;
+//             }
+//             s2Arr.splice(charIndex, 1)
+//         }
+//
+//         return isAnagram;
+//     }
+//
+//     console.assert(!areAnagrams("abc", "abcd"), "Length check is failed");
+//     console.assert(areAnagrams("listen", "silent"), "listen&silent are not anagram");
+//     console.assert(areAnagrams("abc", "cba"), "abc&cba are not anagram");
+//     console.assert(!areAnagrams("abc", "cbd"), "abc&cbd are anagram");
+//     console.assert(areAnagrams("", ""), "empty strings");
+//     console.assert(!areAnagrams("abcсс", "abcaa"), "abcc and abcaa are not anagrams")
+//     console.assert(!areAnagrams("abca", "abcc"), "abcc and abcaa are not anagrams")
+//     console.assert(!areAnagrams("abcaa", "abccc"), "abcc and abcaa are not anagrams")
+//
+//
+//     // HINT: consider exercise33 for code reuse ideas
+// }
+//
+// exerciseExtra1();
